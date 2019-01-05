79e3325699c0202ae9f6110c357aa8b682f11919
invom <invom@qq.com>
2019-01-05 14:18:03 +0800

    Update Material-Kit, Fix TOC Mobile css

--- source/js/material-kit.js
+++ source/js/material-kit.js
@@ -1,7 +1,7 @@
 /*!
 
  =========================================================
- * Material Kit - v2.0.4
+ * Material Kit - v2.0.5
  =========================================================
 
  * Product Page: https://www.creative-tim.com/product/material-kit
