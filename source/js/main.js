1236317655e72dc4be47577156fd069105823370
invom <invom@qq.com>
2019-01-25 15:54:05 +0800

    Add Easter egg in console

--- source/js/main.js
+++ source/js/main.js
@@ -29,4 +29,11 @@ $(window).on('load', function(){
             });
         }  
     }) 
+
+
+    // copy from https://github.com/fi3ework/hexo-theme-archer
+    var logStyle = 'color: #fff; background: #f75357; padding: 1px; border-radius: 5px;'
+    console.log('%c 🎯 Material-T', logStyle)
+    console.log('%c 🏷 Version: 0.9.1 ', logStyle)
+    console.log('%c 📦 https://github.com/invom/Material-T ', logStyle) 
 }) 
