f89ac356ec4c2f58431e8399eddb1ba3d471f199
invom <invom@qq.com>
2019-01-27 21:51:08 +0800

    Optimized structure

--- source/js/main.js
+++ source/js/main.js
@@ -1,36 +1,4 @@
 $(document).ready(function(){  
-    // for Toc
-    var navHeight = $(".navbar").height();
-    var toc = $("#toc");
-    var tocL = toc.offset().left;
-    var tocT = navHeight + $(".material-icons").height();
-    var HHH = $(".main").offset().top;
-    $(window).scroll(function(){  
-        var scroH = toc.scrollTop(); 
-        //var scroH;  
-        // if(document.body.scrollTop){  
-        //     scroH = document.body.scrollTop;  
-        // }  
-        // else{  
-        //     scroH = document.documentElement.scrollTop;  
-        // }  
-        var scroH = document.body.scrollTop + document.documentElement.scrollTop;
-        if(scroH >= HHH){  
-            toc.css({
-                "position": "fixed",
-                "left": tocL,
-                "top": tocT
-            });
-        }else if( scroH < HHH ) {  
-            toc.css({
-                "position": "",
-                "left": '',
-                "top": ''
-            });
-        }  
-    }) 
-
-
     // copy from https://github.com/fi3ework/hexo-theme-archer
     var logStyle = 'color: #fff; background: #f75357; padding: 1px; border-radius: 5px;'
     console.log('%c 🎯 Material-T', logStyle)
---
+++ source/js/post.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){  
+    // for Toc
+    var navHeight = $(".navbar").height();
+    var toc = $("#toc");
+    var tocL = toc.offset().left;
+    var tocT = navHeight + $(".material-icons").height();
+    var HHH = $(".main").offset().top;
+    $(window).scroll(function(){  
+        var scroH = toc.scrollTop(); 
+        //var scroH;  
+        // if(document.body.scrollTop){  
+        //     scroH = document.body.scrollTop;  
+        // }  
+        // else{  
+        //     scroH = document.documentElement.scrollTop;  
+        // }  
+        var scroH = document.body.scrollTop + document.documentElement.scrollTop;
+        if(scroH >= HHH){  
+            toc.css({
+                "position": "fixed",
+                "left": tocL,
+                "top": tocT
+            });
+        }else if( scroH < HHH ) {  
+            toc.css({
+                "position": "",
+                "left": '',
+                "top": ''
+            });
+        }  
+    }) 
+}) 
