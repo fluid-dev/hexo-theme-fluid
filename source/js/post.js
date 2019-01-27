fd1b623e417199efc27918474701208281e4bb78
invom <invom@qq.com>
2019-01-28 01:48:35 +0800

    Add post page layout style && Reuse tocbot

--- source/js/post.js
+++ source/js/post.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){  
-    // for Toc
+    // // for Toc
+    // var navHeight = $(".navbar").height();
+    // var toc = $("#toc");
+    // var tocL = toc.offset().left;
+    // var tocT = navHeight + $(".material-icons").height();
+    // var HHH = $(".main").offset().top;
+    // $(window).scroll(function(){  
+    //     var scroH = toc.scrollTop(); 
+    //     //var scroH;  
+    //     // if(document.body.scrollTop){  
+    //     //     scroH = document.body.scrollTop;  
+    //     // }  
+    //     // else{  
+    //     //     scroH = document.documentElement.scrollTop;  
+    //     // }  
+    //     var scroH = document.body.scrollTop + document.documentElement.scrollTop;
+    //     if(scroH >= HHH){  
+    //         toc.css({
+    //             "position": "fixed",
+    //             "left": tocL,
+    //             "top": tocT
+    //         });
+    //     }else if( scroH < HHH ) {  
+    //         toc.css({
+    //             "position": "",
+    //             "left": '',
+    //             "top": ''
+    //         });
+    //     }  
+    // }) 
+
     var navHeight = $(".navbar").height();
     var toc = $("#toc");
     var tocL = toc.offset().left;
@@ -7,13 +37,6 @@ $(document).ready(function(){
     var HHH = $(".main").offset().top;
     $(window).scroll(function(){  
         var scroH = toc.scrollTop(); 
-        //var scroH;  
-        // if(document.body.scrollTop){  
-        //     scroH = document.body.scrollTop;  
-        // }  
-        // else{  
-        //     scroH = document.documentElement.scrollTop;  
-        // }  
         var scroH = document.body.scrollTop + document.documentElement.scrollTop;
         if(scroH >= HHH){  
             toc.css({
@@ -29,4 +52,15 @@ $(document).ready(function(){
             });
         }  
     }) 
+    tocbot.init({
+        tocSelector: '#tocbot',
+        contentSelector: '.post_content',
+        headingSelector: 'h1, h2, h3, h4, h5, h6',
+        linkClass: 'tocbot-link',
+        activeLinkClass: 'tocbot-active-link',
+        listClass: 'tocbot-list',
+        isCollapsedClass: 'tocbot-is-collapsed',
+        collapsibleClass: 'tocbot-is-collapsible',
+        scrollSmooth: true,
+    });
 }) 
