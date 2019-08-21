24d3dcd9500e099739250ecff7b703e98f24069c
zkqiang <zkqiang@126.com>
2019-08-21 23:01:47 +0800

    :bug: 修复搜索栏的一些BUG 1. 搜索结果的 url 拼接错误 2. 搜索提示没有复位 3. 一些拼写错误

--- scripts/local-search.js
+++ scripts/local-search.js
@@ -1,58 +1,57 @@
 'use strict';
-hexo.extend.generator.register('_hexo_generator_search', function(locals){
-  var config = this.theme.config;
-  if(!config.search.enable) { 
-    return; 
-  }
-
-  var nunjucks = require('nunjucks');
-  var env = new nunjucks.Environment();
-  var pathFn = require('path');
-  var fs = require('fs');
-
-  env.addFilter('uriencode', function(str) {
-    return encodeURI(str);
-  });
-
-  env.addFilter('noControlChars', function(str) {
-    return str && str.replace(/[\x00-\x1F\x7F]/g, '');
-  });
-
-  var searchTmplSrc = pathFn.join(__dirname, '../pages/local-search.xml');
-  var searchTmpl = nunjucks.compile(fs.readFileSync(searchTmplSrc, 'utf8'), env);
-
-  var searchConfig = config.search;
-  var template = searchTmpl;
-  var searchfield = searchConfig.field;
-  var content = searchConfig.content;
-  if (content == undefined) content=true;
-
-  var posts, pages;
-
-  if(searchfield.trim() != ''){
-    searchfield = searchfield.trim();
-    if(searchfield == 'post'){
-      posts = locals.posts.sort('-date');
-    }else if(searchfield == 'page'){
-      pages = locals.pages;
-    }else{
-      posts = locals.posts.sort('-date');
-      pages = locals.pages;
+hexo.extend.generator.register('_hexo_generator_search', function (locals) {
+    var config = this.theme.config;
+    if (!config.search.enable) {
+        return;
     }
-  }else{
-    posts = locals.posts.sort('-date');
-  }
 
-  var xml = template.render({
-    config: config,
-    posts: posts,
-    pages: pages,
-    content: content,
-    url: config.root
-  });
+    var nunjucks = require('nunjucks');
+    var env = new nunjucks.Environment();
+    var pathFn = require('path');
+    var fs = require('fs');
 
-  return {
-    path: searchConfig.path,
-    data: xml
-  };
+    env.addFilter('uriencode', function (str) {
+        return encodeURI(str);
+    });
+
+    env.addFilter('noControlChars', function (str) {
+        return str && str.replace(/[\x00-\x1F\x7F]/g, '');
+    });
+
+    var searchTmplSrc = pathFn.join(__dirname, '../pages/local-search.xml');
+    var searchTmpl = nunjucks.compile(fs.readFileSync(searchTmplSrc, 'utf8'), env);
+
+    var searchConfig = config.search;
+    var template = searchTmpl;
+    var searchField = searchConfig.field;
+    var content = searchConfig.content || true;
+
+    var posts, pages;
+
+    if (searchField.trim() != '') {
+        searchField = searchField.trim();
+        if (searchField == 'post') {
+            posts = locals.posts.sort('-date');
+        } else if (searchField == 'page') {
+            pages = locals.pages;
+        } else {
+            posts = locals.posts.sort('-date');
+            pages = locals.pages;
+        }
+    } else {
+        posts = locals.posts.sort('-date');
+    }
+
+    var xml = template.render({
+        config: config,
+        posts: posts,
+        pages: pages,
+        content: content,
+        url: config.root
+    });
+
+    return {
+        path: searchConfig.path,
+        data: xml
+    };
 });
