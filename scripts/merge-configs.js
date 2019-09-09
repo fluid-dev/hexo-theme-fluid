4b2c8603045b49583c926c67cec21f37e8c1b7ff
zkqiang <zkqiang@126.com>
2019-09-09 12:16:53 +0800

    :beers: 更名并迁移至组织

--- scripts/merge-configs.js
+++ scripts/merge-configs.js
@@ -1,14 +1,14 @@
 const merge = require('./merge');
 
 /**
- * Merge configs in blog/source/_data/material_t_config.yml into hexo.theme.config.
+ * Merge configs in blog/source/_data/fluid_config.yml into hexo.theme.config.
  */
 hexo.on('generateBefore', function () {
   if (!hexo.locals.get) return;
   var data = hexo.locals.get('data');
   if (!data) return;
 
-  if (data.material_t_config) {
-    merge(hexo.theme.config, data.material_t_config);
+  if (data.fluid_config) {
+    merge(hexo.theme.config, data.fluid_config);
   }
 });
