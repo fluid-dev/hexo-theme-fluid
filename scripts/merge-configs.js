0902a1a32fec99480cff7f44e3fb9ee12e582066
Rook1e <rook1e404@outlook.com>
2019-09-22 22:40:02 +0800

    :bug: 完善配置合并和懒加载

--- scripts/merge-configs.js
+++ scripts/merge-configs.js
@@ -1,23 +1,10 @@
 const fs = require('fs');
 const path = require('path');
-const yaml = require('./utils/js-yaml');
-const merge = require('./utils/merge');
+const yaml = require('js-yaml');
 
 /**
- * Merge configs in blog/source/_data/fluid_config.yml into hexo.theme.config.
+ * Merge configs in /source/_data/fluid_static_prefix.yml into hexo.theme.config.
  */
-hexo.on('ready', function () {
-  var sourceConfig = {}
-  if(fs.existsSync(configPath = path.join(__dirname, '../../../source/_data/fluid_config.yml'))) {
-    sourceConfig = yaml.safeLoad(fs.readFileSync(configPath, 'utf8'));
-  }
-  themeConfig = yaml.safeLoad(fs.readFileSync(path.join(__dirname, '../_config.yml'), 'utf8'));
-  hexo.theme.config = Object.assign({}, themeConfig, sourceConfig, hexo.config.theme_config);
-  hexo.config.theme_config = Object.assign({}, sourceConfig, hexo.config.theme_config);
-
-  
-  require("./lazyload/index").lazyload(hexo);
-});
 
 hexo.on('generateBefore', function () {
   if (hexo.locals.get) {
@@ -26,6 +13,10 @@ hexo.on('generateBefore', function () {
       hexo.theme.config.static_prefix = data.fluid_static_prefix;
       this.log.debug("Fluid: theme static_prefix config merged");
     }
+    sourceConfig = {}
+    if (data && data.fluid_config) {
+      sourceConfig = data.fluid_config;
+    }
   }
   if (!hexo.theme.config.static_prefix) {
     const configPath = path.join(__dirname, '../_static_prefix.yml');
@@ -33,5 +24,16 @@ hexo.on('generateBefore', function () {
     hexo.theme.config.static_prefix = doc;
     this.log.debug("Fluid: theme static_prefix config merged");
   }
+
+/**
+ * Merge configs in hexo.config.theme_config and /source/_data/fluid_config.yml into hexo.theme.config.
+ */
+  hexo.theme.config = Object.assign({}, hexo.theme.config, sourceConfig, hexo.config.theme_config);
+  this.log.debug("Fluid: theme config merged");
+
+/**
+ * Trigger action that requires configuration data.
+ */
+  require("./lazyload").lazyload(hexo)
   return;
 });
