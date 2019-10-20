1a65e7e8193872c4627211c17806d4e41a97b821
zkqiang <zkqiang@126.com>
2019-10-21 00:59:23 +0800

    :bug: 修复静态资源配置没有合并

--- scripts/merge-configs.js
+++ scripts/merge-configs.js
@@ -2,38 +2,35 @@ const fs = require('fs');
 const path = require('path');
 const yaml = require('js-yaml');
 
-/**
- * Merge configs in /source/_data/fluid_static_prefix.yml into hexo.theme.config.
- */
-
 hexo.on('generateBefore', function () {
   if (hexo.locals.get) {
     var data = hexo.locals.get('data');
-    if (data && data.fluid_static_prefix) {
-      hexo.theme.config.static_prefix = data.fluid_static_prefix;
-      this.log.debug("Fluid: theme static_prefix config merged");
-    }
-    sourceConfig = {}
+    sourceConfig = {};
     if (data && data.fluid_config) {
       sourceConfig = data.fluid_config;
     }
-  }
-  if (!hexo.theme.config.static_prefix) {
-    const configPath = path.join(__dirname, '../_static_prefix.yml');
-    const doc = yaml.safeLoad(fs.readFileSync(configPath, 'utf8'));
-    hexo.theme.config.static_prefix = doc;
-    this.log.debug("Fluid: theme static_prefix config merged");
+    staticPrefix = {};
+    if (data && data.fluid_static_prefix) {
+      staticPrefix = data.fluid_static_prefix;
+    }
   }
 
-/**
- * Merge configs in hexo.config.theme_config and /source/_data/fluid_config.yml into hexo.theme.config.
- */
+  /**
+   * Merge configs in /source/_data/fluid_static_prefix.yml into hexo.theme.config.
+   */
+  const configPath = path.join(__dirname, '../_static_prefix.yml');
+  const yamlDoc = yaml.safeLoad(fs.readFileSync(configPath, 'utf8'));
+  hexo.theme.config.static_prefix = Object.assign({}, yamlDoc, staticPrefix);
+  this.log.debug("Fluid: theme static_prefix config merged");
+
+  /**
+   * Merge configs in hexo.config.theme_config and /source/_data/fluid_config.yml into hexo.theme.config.
+   */
   hexo.theme.config = Object.assign({}, hexo.theme.config, sourceConfig, hexo.config.theme_config);
   this.log.debug("Fluid: theme config merged");
 
-/**
- * Trigger action that requires configuration data.
- */
-  require("./lazyload").lazyload(hexo)
-  return;
+  /**
+   * Trigger action that requires configuration data.
+   */
+  require("./lazyload").lazyload(hexo);
 });
