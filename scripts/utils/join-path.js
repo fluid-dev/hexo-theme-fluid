9592e4094b40b7dce795db0171bcd497bc94e31e
zkqiang <zkqiang@126.com>
2020-02-19 15:47:01 +0800

    :art: 优化图片链接拼接

--- scripts/utils/join-path.js
+++ scripts/utils/join-path.js
@@ -1,4 +1,7 @@
 const joinPath = function (base, relative) {
+  if (relative && /^https*:\/\//.test(relative)) {
+    return relative;
+  }
   return relative
     ? base.replace(/\/+$/, '') + '/' + relative.replace(/^\/+/, '')
     : base;
