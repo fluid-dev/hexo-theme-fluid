7139b386b533fa3915b8f6cb6c273161f3e92601
zkqiang <zkqiang@126.com>
2020-03-04 22:05:05 +0800

    :art: 优化统计数据的加载

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,13 +1,9 @@
 const joinPath = require('./utils/join-path');
 
 hexo.extend.helper.register('css_ex', function (base, relative, ex = "") {
-  return '<link rel="stylesheet" href="' + this.url_for(joinPath(base, relative)) + '" ' + ex + ' >';
+  return `<link ${ ex } rel="stylesheet" href="${ this.url_for(joinPath(base, relative)) }" />`;
 });
 
 hexo.extend.helper.register('js_ex', function (base, relative, ex = "") {
-  return '<script src="' + this.url_for(joinPath(base, relative)) + '" ' + ex + '></script>';
-});
-
-hexo.extend.helper.register('js_async', function (base, relative, ex = "") {
-  return '<script async src="' + this.url_for(joinPath(base, relative)) + '" ' + ex + '></script>';
+  return `<script ${ ex } src="${ this.url_for(joinPath(base, relative)) }" ></script>`;
 });
