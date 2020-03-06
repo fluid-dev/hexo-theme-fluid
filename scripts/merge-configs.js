6ea04f48ba89bda3dc326169ce7740393e06e0d5
zkqiang <zkqiang@126.com>
2020-03-06 21:27:01 +0800

    :zap: 强制禁用 Hexo 高亮

--- scripts/merge-configs.js
+++ scripts/merge-configs.js
@@ -4,8 +4,11 @@ const yaml = require('js-yaml');
 const _ = require('./utils/lodash');
 
 hexo.on('generateBefore', function () {
+  let sourceConfig;
+  let staticPrefix;
+
   if (hexo.locals.get) {
-    var data = hexo.locals.get('data');
+    const data = hexo.locals.get('data');
     sourceConfig = {};
     if (data && data.fluid_config) {
       sourceConfig = data.fluid_config;
@@ -16,25 +19,23 @@ hexo.on('generateBefore', function () {
     }
   }
 
-  /**
-   * Merge configs in /source/_data/fluid_static_prefix.yml into hexo.theme.config.
-   */
+  // Force disable hexo highlight
+  if (hexo.theme.config.highlight.enable) {
+    hexo.config.highlight.enable = false;
+  }
+
+  // Merge configs in /source/_data/fluid_static_prefix.yml into hexo.theme.config.
   const configPath = path.join(__dirname, '../_static_prefix.yml');
   const yamlDoc = yaml.safeLoad(fs.readFileSync(configPath, 'utf8'));
   hexo.theme.config.static_prefix = _.merge({}, yamlDoc, staticPrefix);
   this.log.debug('Fluid: theme static_prefix config merged');
 
-  /**
-   * Merge configs in hexo.config.theme_config and /source/_data/fluid_config.yml into hexo.theme.config.
-   */
+  // Merge configs in hexo.config.theme_config and /source/_data/fluid_config.yml into hexo.theme.config.
   hexo.theme.config = _.merge({}, hexo.theme.config, sourceConfig, hexo.config.theme_config);
   this.log.debug('Fluid: theme config merged');
 
-  /**
-   * Trigger action that requires configuration data.
-   */
-  require("./lazyload").lazyload(hexo);
-
-  this.log.debug("Configs:\n", hexo.theme.config);
+  // Trigger action that requires configuration data.
+  require('./lazyload').lazyload(hexo);
+  this.log.debug('Configs:\n', hexo.theme.config);
 
 });
