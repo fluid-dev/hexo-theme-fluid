d04587afd2e14fda27e6ea4d66c8ece2d930f759
zkqiang <zkqiang@126.com>
2020-03-19 01:22:34 +0800

    :sparkles: 代码块增加复制按钮

---
+++ source/js/clipboard-use.js
@@ -0,0 +1,26 @@
+!function (e, t, a) {
+  var initCopyCode = function () {
+    var copyHtml = '';
+    copyHtml += '<button class="copy-btn" data-clipboard-snippet="">';
+    copyHtml += '<i class="far fa-copy"></i><span>Copy</span>';
+    copyHtml += '</button>';
+    $('pre.highlight').wrap($('<div class="code-block"></div>'));
+    $('.code-block').prepend(copyHtml);
+    var clipboard = new ClipboardJS('.copy-btn', {
+      target: function (trigger) {
+        return trigger.nextElementSibling;
+      },
+    });
+    clipboard.on('success', function (e) {
+      e.trigger.innerHTML = 'Success';
+      setTimeout(function () {
+        e.trigger.outerHTML = copyHtml;
+      }, 2000);
+    });
+  };
+  var oldLoad = window.onload;
+  window.onload = function () {
+    initCopyCode();
+    oldLoad && oldLoad();
+  };
+}(window, document);
