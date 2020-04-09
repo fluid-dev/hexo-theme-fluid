d19f3fae77bc5ec048ad2d93c5c18fb0d07893ef
zkqiang <zkqiang@126.com>
2020-04-09 15:51:26 +0800

    :bug: 修复列表配置项的合并

--- scripts/merge-configs.js
+++ scripts/merge-configs.js
@@ -31,11 +31,16 @@ hexo.on('generateBefore', function () {
   this.log.debug('Fluid: theme static_prefix config merged');
 
   // Merge configs in hexo.config.theme_config and /source/_data/fluid_config.yml into hexo.theme.config.
-  hexo.theme.config = _.merge({}, hexo.theme.config, sourceConfig, hexo.config.theme_config);
+  hexo.theme.config = _.mergeWith({}, hexo.theme.config, sourceConfig, hexo.config.theme_config,
+    function (objValue, srcValue) {
+      if (_.isArray(objValue)) {
+        return srcValue;
+      }
+    });
   this.log.debug('Fluid: theme config merged');
 
   // Trigger action that requires configuration data.
   require('./lazyload').lazyload(hexo);
-  this.log.debug('Configs:\n', hexo.theme.config);
+  this.log.debug('Configs:\n', JSON.stringify(hexo.theme.config, undefined, 2));
 
 });
