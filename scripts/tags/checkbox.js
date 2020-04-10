2564d71a5d5a5b2946cc56757e934f7065c2c84b
zkqiang <zkqiang@126.com>
2020-04-10 11:39:51 +0800

    :art: 优化复选框插件代码

--- scripts/tags/checkbox.js
+++ scripts/tags/checkbox.js
@@ -1,16 +1,17 @@
 'use strict';
 
 function checkbox(args) {
+  args = args[0] === ',' ? args.slice(1) : args;
   args = args.join(' ').split(',');
   let text = args[0] || '';
-  let checked = args[1] || '';
-  let inline = (args[2] || '').length;
+  let checked = (args[1] || '').length > 0 && args[1].trim() !== 'false';
+  let inline = (args[2] || '').length > 0 && args[2].trim() !== 'false';
 
-  return `${ !inline ? '<div>' : ''}
-            <input type="checkbox" ${ checked.length > 0 ? 'checked="checked"' : '' }></input>${ text }
-          ${ !inline ? '</div>' : ''}`;
+  return `${ !inline ? '<div>' : '' }
+            <input type="checkbox" ${ checked ? 'checked="checked"' : '' }>${ text }</input>
+          ${ !inline ? '</div>' : '' }`;
 }
 
-// {% cb, text, inline? %}
-hexo.extend.tag.register('checkbox', checkbox, {ends: false});
-hexo.extend.tag.register('cb', checkbox, {ends: false});
+// {% cb text, checked?, inline? %}
+hexo.extend.tag.register('checkbox', checkbox, { ends: false });
+hexo.extend.tag.register('cb', checkbox, { ends: false });
