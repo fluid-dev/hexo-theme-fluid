22c3f6efcb53508892e70d427e3ad02801146a3b
zkqiang <zkqiang@126.com>
2020-04-11 13:57:52 +0800

    :bug: mermaid 代码不显示复制按钮

--- source/js/clipboard-use.js
+++ source/js/clipboard-use.js
@@ -4,8 +4,13 @@
     copyHtml += '<button class="copy-btn" data-clipboard-snippet="">';
     copyHtml += '<i class="iconfont icon-copy"></i><span>Copy</span>';
     copyHtml += '</button>';
-    var pre = $('.markdown-body pre');
-    pre.prepend(copyHtml);
+    $('.markdown-body pre').each(function () {
+      const pre = $(this);
+      if (pre.find('code.mermaid').length > 0) {
+        return;
+      }
+      pre.append(copyHtml);
+    });
     var clipboard = new ClipboardJS('.copy-btn', {
       target: function (trigger) {
         return trigger.nextElementSibling;
