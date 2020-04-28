d0c329ae7c712f6f095d6f36cde7186277056413
zkqiang <zkqiang@126.com>
2020-04-28 23:32:39 +0800

    :sparkles: 增加欢迎日志

--- scripts/events/index.js
+++ scripts/events/index.js
@@ -3,6 +3,7 @@
 'use strict';
 
 hexo.on('generateBefore', () => {
+  require('./lib/hello')(hexo);
   require('./lib/merge-configs')(hexo);
   require('./lib/highlight')(hexo);
   require('./lib/lazyload')(hexo);
