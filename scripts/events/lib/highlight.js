ab75ab7d8ed23f994a5929ef45469bc7c86454aa
zkqiang <zkqiang@126.com>
2020-05-03 22:04:44 +0800

    :art: 优化代码高亮配置

--- scripts/events/lib/highlight.js
+++ scripts/events/lib/highlight.js
@@ -1,18 +1,21 @@
 'use strict';
 
+const objUtil = require('../../utils/object');
+
 module.exports = (hexo) => {
-  var config = hexo.theme.config;
+  const config = hexo.theme.config;
   if (!config.highlight.enable) {
     return;
   }
 
   // Force set hexo config
-  hexo.config.highlight = {
+  hexo.config.highlight = objUtil.merge({}, hexo.config.highlight, {
     enable     : true,
     hljs       : true,
     line_number: false,
-    wrap       : false
-  };
+    wrap       : false,
+    auto_detect: true
+  });
 
   if (config.highlight.bg_color) {
     hexo.extend.filter.register('after_render:html', (html, data) => {
