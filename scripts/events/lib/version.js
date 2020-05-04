3e2f53f0ebea52b0268bf34958681a3bdf06b4c4
zkqiang <zkqiang@126.com>
2020-05-04 15:26:33 +0800

    :zap: 兼容低版本 Node

--- scripts/events/lib/version.js
+++ scripts/events/lib/version.js
@@ -18,11 +18,7 @@ module.exports = (hexo) => {
     }
   };
 
-  https.get('https://api.github.com/repos/fluid-dev/hexo-theme-fluid/releases/latest', {
-    headers: {
-      'User-Agent': 'Theme Fluid Client'
-    }
-  }, (res) => {
+  https.get('https://api.github.com/repos/fluid-dev/hexo-theme-fluid/releases/latest', (res) => {
     let result = '';
     res.on('data', data => {
       result += data;
