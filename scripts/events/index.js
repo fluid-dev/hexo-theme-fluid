4d4e760388c5f87bc964bb9760c44404b55a09da
zkqiang <zkqiang@126.com>
2020-05-04 15:23:01 +0800

    :fire: 移除 Node 版本检测

--- scripts/events/index.js
+++ scripts/events/index.js
@@ -4,7 +4,6 @@
 
 hexo.on('generateBefore', () => {
   require('./lib/hello')(hexo);
-  require('./lib/check')(hexo);
   require('./lib/merge-configs')(hexo);
   require('./lib/highlight')(hexo);
   require('./lib/lazyload')(hexo);
