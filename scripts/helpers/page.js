0e7d8ec39617fd68faba5a11f6698cce0308ccbb
zkqiang <zkqiang@126.com>
2020-10-17 18:18:51 +0800

    :art: 重命名一处函数

--- scripts/helpers/page.js
+++ scripts/helpers/page.js
@@ -2,24 +2,24 @@
 
 'use strict';
 
-hexo.extend.helper.register('prev_page', function prev_page(page) {
-  const prev = page.prev;
+hexo.extend.helper.register('prev_post', function prev_post(post) {
+  const prev = post.prev;
   if (!prev) {
     return null;
   }
   if (prev.hide) {
-    return prev_page(prev);
+    return prev_post(prev);
   }
   return prev;
 });
 
-hexo.extend.helper.register('next_page', function next_page(page) {
-  const next = page.next;
+hexo.extend.helper.register('next_post', function next_post(post) {
+  const next = post.next;
   if (!next) {
     return null;
   }
   if (next.hide) {
-    return next_page(next);
+    return next_post(next);
   }
   return next;
 });
