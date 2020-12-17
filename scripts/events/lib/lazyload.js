754ed97a0926c66cf788e6dad6c9addc0c24ba23
zkqiang <zkqiang@126.com>
2020-12-17 19:18:18 +0800

    :art: 规范代码

--- scripts/events/lib/lazyload.js
+++ scripts/events/lib/lazyload.js
@@ -3,13 +3,13 @@
 const joinPath = require('../../utils/join-path');
 
 module.exports = (hexo) => {
-  var config = hexo.theme.config;
+  const config = hexo.theme.config;
   let loadingImage = joinPath(joinPath(hexo.config.root, config.static_prefix.internal_img), 'loading.gif');
   if (!config.lazyload || !config.lazyload.enable || !loadingImage) {
     return;
   }
   if (config.lazyload.onlypost) {
-    hexo.extend.filter.register('after_post_render', function(page) {
+    hexo.extend.filter.register('after_post_render', (page) => {
       if (page.layout !== 'post' && !page.lazyload) {
         return;
       }
@@ -19,9 +19,9 @@ module.exports = (hexo) => {
       return page;
     });
   } else {
-    hexo.extend.filter.register('after_render:html', function(str, data) {
+    hexo.extend.filter.register('after_render:html', (html, data) => {
       if (!data.page || data.page.lazyload !== false) {
-        return lazyProcess(str, loadingImage);
+        return lazyProcess(html, loadingImage);
       }
     });
   }
