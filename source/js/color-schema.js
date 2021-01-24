c1b2d6904782a691cfcb5d4614b46053be24eeed
bubao <asd565586630@gmail.com>
2021-01-24 23:21:34 +0800

    移除主题中 Shoka 换皮动画功能，使用注入器实现

--- source/js/color-schema.js
+++ source/js/color-schema.js
@@ -203,40 +203,7 @@
     if (button) {
       // 当用户点击切换按钮时，获得新的显示模式、写入 localStorage、并在页面上生效
       button.addEventListener('click', () => {
-		const BODY = document.getElementsByTagName('body')[0];
-
-		var neko = BODY.createChild('div', {
-            id: 'neko',
-            innerHTML: '<div class="planet"><div class="sun"></div><div class="moon"></div></div><div class="body"><div class="face"><section class="eyes left"><span class="pupil"></span></section><section class="eyes right"><span class="pupil"></span></section><span class="nose"></span></div></div>'
-        });
-
-        var hideNeko = function() {
-            Fluid.utils.transition(neko, {
-                delay: 2500,
-                opacity: 0
-            }, function() {
-                BODY.removeChild(neko)
-            });
-        }
-		var currentSetting = getLS(colorSchemaStorageKey);
-        if (currentSetting==="light") {
-            var c = function() {
-				neko.addClass('dark');
-				applyCustomColorSchemaSettings(toggleCustomColorSchema());
-                hideNeko();
-            };
-        } else {
-            neko.addClass('dark');
-            var c = function() {
-				neko.removeClass('dark');
-				applyCustomColorSchemaSettings(toggleCustomColorSchema());
-                hideNeko();
-            };
-        }
-        Fluid.utils.transition(neko, 1, function() {
-            setTimeout(c, 210)
-        });
-        
+		applyCustomColorSchemaSettings(toggleCustomColorSchema());
       });
     }
   });
