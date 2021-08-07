7bd6f38f526df1cc358943341762b6651fc46946
zkqiang <zkqiang@126.com>
2021-08-07 15:34:57 +0800

    :zap: 引入版本号比较工具

--- scripts/helpers/utils.js
+++ scripts/helpers/utils.js
@@ -3,7 +3,12 @@
 'use strict';
 
 const crypto = require('crypto');
+const compareVersions = require('../../scripts/utils/compare-versions');
 
 hexo.extend.helper.register('md5', function(string) {
   return crypto.createHash('md5').update(string).digest('hex');
+});
+
+hexo.extend.helper.register('compareVersions', function(ver1, ver2) {
+  return compareVersions(ver1, ver2);
 });
