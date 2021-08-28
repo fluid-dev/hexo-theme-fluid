bd1e34dd2151980c6344b9b81f7d406ac0f38148
zkqiang <zkqiang@126.com>
2021-08-28 21:50:15 +0800

    :zap: LeanCloud 统计可以指定路径参数 #514

--- source/js/leancloud.js
+++ source/js/leancloud.js
@@ -123,7 +123,8 @@
     // 如果有页面浏览数节点，则请求浏览数并自增
     var viewCtn = document.querySelector('#leancloud-page-views-container');
     if (viewCtn) {
-      var target = decodeURI(window.location.pathname.replace(/\/*(index.html)?$/, '/'));
+      var path = eval(CONFIG.web_analytics.leancloud.path || 'window.location.pathname');
+      var target = decodeURI(path.replace(/\/*(index.html)?$/, '/'));
       var viewGetter = getRecord(Counter, target).then((record) => {
         enableIncr && incrArr.push(buildIncrement(record.objectId));
         if (viewCtn) {
