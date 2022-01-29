9a9cf1eb6f9d72e5aafd0098eaa8ca3698aa4569
zkqiang <zkqiang@126.com>
2022-01-30 02:38:09 +0800

    :art: 重命名 partials 目录名

--- scripts/filters/default-inject.js
+++ scripts/filters/default-inject.js
@@ -5,18 +5,18 @@
 const path = require('path');
 
 hexo.extend.filter.register('theme_inject', function(injects) {
-  injects.header.file('default', path.join(hexo.theme_dir, 'layout/_partial/header.ejs'));
-  injects.footer.file('default', path.join(hexo.theme_dir, 'layout/_partial/footer.ejs'));
+  injects.header.file('default', path.join(hexo.theme_dir, 'layout/_partials/header.ejs'));
+  injects.footer.file('default', path.join(hexo.theme_dir, 'layout/_partials/footer.ejs'));
 
-  injects.postLeft.file('default', path.join(hexo.theme_dir, 'layout/_partial/post/sidebar-left.ejs'));
-  injects.postRight.file('default', path.join(hexo.theme_dir, 'layout/_partial/post/sidebar-right.ejs'));
-  injects.postMetaHeader.file('default', path.join(hexo.theme_dir, 'layout/_partial/post/meta-header.ejs'));
+  injects.postLeft.file('default', path.join(hexo.theme_dir, 'layout/_partials/post/sidebar-left.ejs'));
+  injects.postRight.file('default', path.join(hexo.theme_dir, 'layout/_partials/post/sidebar-right.ejs'));
+  injects.postMetaHeader.file('default', path.join(hexo.theme_dir, 'layout/_partials/post/meta-header.ejs'));
   if (hexo.theme.config.post.copyright.enable) {
-    injects.postCopyright.file('default', path.join(hexo.theme_dir, 'layout/_partial/post/copyright.ejs'));
+    injects.postCopyright.file('default', path.join(hexo.theme_dir, 'layout/_partials/post/copyright.ejs'));
   }
   if (hexo.theme.config.post.comments.enable) {
-    injects.postComments.file('default', path.join(hexo.theme_dir, 'layout/_partial/comments.ejs'));
+    injects.postComments.file('default', path.join(hexo.theme_dir, 'layout/_partials/comments.ejs'));
   }
 
-  injects.pageComments.file('default', path.join(hexo.theme_dir, 'layout/_partial/comments.ejs'));
+  injects.pageComments.file('default', path.join(hexo.theme_dir, 'layout/_partials/comments.ejs'));
 }, -99);
