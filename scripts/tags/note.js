606f0235a3c424e6a6e12c6ad448ea988b0b8360
wangdekun <wangdekunemail@gmail.com>
2023-05-10 21:06:47 +0800

    :bug: 修复提示标签默认值不生效

--- scripts/tags/note.js
+++ scripts/tags/note.js
@@ -3,8 +3,8 @@
 'use strict';
 
 const note = (args, content) => {
-  if (!args || !args[0]) {
-    args = ['default'];
+  if (!args || !args[0] || args[0].toLowerCase() === "default") {
+    args = [ hexo.theme.config.post.updated.note_class || "info"];
   }
   return `<div class="note note-${args.join(' ')}">
             ${hexo.render.renderSync({ text: content, engine: 'markdown' }).split('\n').join('')}
