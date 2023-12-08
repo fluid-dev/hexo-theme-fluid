606b57b4af2a7faa9fd642dc54b4bfa98b37fa90
zkqiang <zkqiang@126.com>
2023-12-08 11:54:07 +0800

    :zap: Checkbox Tag 默认 enabled

--- scripts/tags/checkbox.js
+++ scripts/tags/checkbox.js
@@ -16,14 +16,14 @@ const checkbox = (args) => {
 
   const checked = (args[1] || '').length > 0 && args[1].trim() !== 'false';
   const inline = (args[2] || '').length > 0 && args[2].trim() !== 'false';
-  const enabled = (args[3] || '').length > 0 && args[3].trim() === 'false';
+  const disabled = (args[3] || '').length > 0 && args[3].trim() !== 'false';
 
   return `${!inline ? '<div>' : ''}
-            <input type="checkbox" ${enabled ? '' : 'disabled'} ${checked ? 'checked="checked"' : ''}>${text}
+            <input type="checkbox" ${disabled ? 'disabled' : ''} ${checked ? 'checked="checked"' : ''}>${text}
           ${!inline ? '</div>' : ''}`;
 
 };
 
-// {% cb text, checked?, inline? %}
+// {% cb text, checked?, inline?, disabled? %}
 hexo.extend.tag.register('checkbox', checkbox, { ends: false });
 hexo.extend.tag.register('cb', checkbox, { ends: false });
