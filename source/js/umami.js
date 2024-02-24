8ef03dfea9c9ac6d0abd742a0b8d4c4e4ae9920e
mobeicanyue <81098819+mobeicanyue@users.noreply.github.com>
2024-02-24 22:38:51 +0800

    add umami pv and uv

---
+++ source/js/umami.js
@@ -0,0 +1,72 @@
+// 从配置文件中获取 umami 的配置
+const website_id = CONFIG.web_analytics.umami.data_website_id;
+// 拼接请求地址
+const request_url =
+  CONFIG.web_analytics.umami.api_server +
+  "/api/websites/" +
+  website_id +
+  "/stats";
+const start_time = new Date(CONFIG.web_analytics.umami.start_time).getTime(); // 转换为 Unix 时间戳
+const end_time = new Date().getTime();
+const token = CONFIG.web_analytics.umami.token;
+
+// 检查配置是否为空
+if (!website_id) {
+  throw new Error("Umami website_id is empty");
+}
+if (!request_url) {
+  throw new Error("Umami request_url is empty");
+}
+if (!start_time) {
+  throw new Error("Umami start_time is empty");
+}
+if (!token) {
+  throw new Error("Umami token is empty");
+}
+
+// 构造请求参数
+const params = new URLSearchParams({
+  startAt: start_time,
+  endAt: end_time,
+});
+// 构造请求头
+const request_header = {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + token,
+    'Access-Control-Allow-Origin': '*' // 允许跨域
+  },
+};
+
+async function allStats() {
+  try {
+    const response = await fetch(`${request_url}?${params}`, request_header);
+    const data = await response.json();
+    const uniqueVisitors = data.uniques.value; // 获取独立访客数
+    const pageViews = data.pageviews.value; // 获取页面浏览量
+
+    let pvCtn = document.querySelector("#umami-site-pv-container");
+    if (pvCtn) {
+      let ele = document.querySelector("#umami-site-pv");
+      if (ele) {
+        ele.textContent = pageViews; // 设置页面浏览量
+        pvCtn.style.display = "inline"; // 将元素显示出来
+      }
+    }
+
+    let uvCtn = document.querySelector("#umami-site-uv-container");
+    if (uvCtn) {
+      let ele = document.querySelector("#umami-site-uv");
+      if (ele) {
+        ele.textContent = uniqueVisitors;
+        uvCtn.style.display = "inline";
+      }
+    }
+  } catch (error) {
+    console.error(error);
+    return "-1";
+  }
+}
+
+allStats();
