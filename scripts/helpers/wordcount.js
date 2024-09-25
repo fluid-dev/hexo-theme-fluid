568cbf84b2698f0c279c22a0824429fba4b93d60
zkqiang <zkqiang@126.com>
2024-09-25 15:21:01 +0800

    :zap: 简化字数统计

--- scripts/helpers/wordcount.js
+++ scripts/helpers/wordcount.js
@@ -5,13 +5,10 @@
 const { stripHTML } = require('hexo-util');
 
 const getWordCount = (post) => {
-  // post.origin is the original post content of hexo-blog-encrypt
-  const content = stripHTML(post.origin || post.content).replace(/\r?\n|\r/g, '').replace(/\s+/g, '');
-
   if (!post.wordcount) {
-    const zhCount = (content.match(/[\u4E00-\u9FA5]/g) || []).length;
-    const enCount = (content.replace(/[\u4E00-\u9FA5]/g, '').match(/[a-zA-Z0-9_\u0392-\u03c9\u0400-\u04FF]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af\u0400-\u04FF]+|[\u00E4\u00C4\u00E5\u00C5\u00F6\u00D6]+|\w+/g) || []).length;
-    post.wordcount = zhCount + enCount
+    // post.origin is the original post content of hexo-blog-encrypt
+    const content = stripHTML(post.origin || post.content).replace(/[\s\r\n]/g, '');
+    post.wordcount = content.length;
   }
   return post.wordcount;
 };
